5f4c6adf03be9e9a631ca6945dfbdd16d4352ae8
Benjamin <benjamin.petelinek@gmail.com>
2024-03-09 13:25:35 +0100

    nov

--- notebooks/Naloga01-IIS/test.py
+++ notebooks/Naloga01-IIS/test.py
@@ -8,7 +8,7 @@ class TestCodeQuality(unittest.TestCase):
         result = subprocess.run(['flake8', '--exclude', '.git,__pycache__,venv', '--count', '--select=E9,F63,F7,F82', '--show-source', '--statistics'], stdout=subprocess.PIPE)
 
         # Če je število najdenih napak večje od 0, test pade
-        self.assertEqual(result.returncode, 0, f"Koda ni v redu. Povzetek napak: {result.stdout.decode('utf-8')}")
+        self.assertEqual(result.returncode, 300, f"Koda ni v redu. Povzetek napak: {result.stdout.decode('utf-8')}")
 
 
 if __name__ == '__main__':
